794656c2482e51921bc07793a656e40a20048d80
cleabrayan <cleabrayan@gmail.com>
2022-04-10 06:02:19 -0400

    Added page lilmit

--- js/hrate.js
+++ js/hrate.js
@@ -1,4 +1,4 @@
-const hrate_MOVIES_API='https://api.themoviedb.org/3/discover/movie/?certification_country=US&certification=R&sort_by=vote_average.desc&api_key=f59db0d7df317b31f4207e1c560b99c0'
+const hrate_MOVIES_API='https://api.themoviedb.org/3/discover/movie/?certification_country=US&certification=R&sort_by=vote_average.desc&api_key=f59db0d7df317b31f4207e1c560b99c0&page2'
 const hrate_img = 'https://image.tmdb.org/t/p/w500';
 const hrate = document.getElementById('hrate');
 fetchhrate(hrate_MOVIES_API)
